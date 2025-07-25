a905c8f1c45339257ab1a3c6b27cbbffae80184f
ntatayamiko <ntatayamiko@gmail.com>
2025-07-24 18:58:42 -0600

    UI layout

--- rpcontacts/views.py
+++ rpcontacts/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """This module provides views to manage the contacts table."""
 
-from PyQt5.QtWidgets import (QHBoxLayout, QMainWindow, QWidget)
+from PyQt5.QtWidgets import (QAbstractItemView,QHBoxLayout, QMainWindow, QPushButton, QTableView, QVBoxLayout, QWidget,)
 
 class Window(QMainWindow):
     """Main Window"""
@@ -14,3 +14,23 @@ class Window(QMainWindow):
         self.setCentralWidget(self.centralWidget)
         self.layout = QHBoxLayout()
         self.centralWidget.setLayout(self.layout)
+        self.setupUI()
+
+    def setupUI(self):
+        """ Setup the main window's GUI."""
+        # Create a table view widget
+        self.table = QTableView()
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.resizeColumnsToContents()
+        #Create buttons
+        self.addButton = QPushButton("Add...")
+        self.deleteButton = QPushButton("Delete")
+        self.clearAllButton = QPushButton("Clear All")
+        #Lay out the GUI
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addStretch()
+        layout.addWidget(self.clearAllButton)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
