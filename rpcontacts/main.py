f7e23dba1720e094d09bd2a2aa87d964a16a1342
ntatayamiko <ntatayamiko@gmail.com>
2025-07-24 21:41:19 -0600

    few good lines

--- rpcontacts/main.py
+++ rpcontacts/main.py
@@ -4,11 +4,15 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication
+from .database import createConnection
 from .views import Window
 
 def main():
     """RP Contacts main function"""
     app=QApplication(sys.argv)
+    #conect to database before creating any window
+    if not createConnection("contacts.sqlite"):
+        sys.exit(1)
     #Create the main window
     win=Window()
     win.show()
